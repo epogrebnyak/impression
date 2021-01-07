86654b3a92a232b3c54dc0e6207ecd37ad43d462
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-07 22:08:13 +0300

    use a hook

--- {{cookiecutter.package_name}}/hooks/pyproject.py
+++ {{cookiecutter.package_name}}/hooks/pyproject.py
@@ -4,20 +4,26 @@ Based on Poetry mechanism to split authors name and email from pyproject.toml:
     https://github.com/python-poetry/poetry/blob/35058edf97e24ef1ac2b77563c84eed66d46939e/poetry/packages/package.py
 
 """
+import os
 import toml
 import re
 from typing import Tuple
 
-AUTHOR_REGEX = re.compile(r"(?u)^(?P<name>[- .,\w\d'’\"()]+)(?: <(?P<email>.+?)>)?$")
+AUTHOR_REGEX = re.compile(r"^(?P<name>[- .,\w\d'’\"()\{\}]+)(?: <(?P<email>.+?)>)?$")
+PATH = os.path.join("..", "pyproject.toml")
 
-
-def get_author(s: str) -> Tuple[str, str]:
+def extract_author(s: str) -> Tuple[str, str]:
     m = AUTHOR_REGEX.match(s)
     name = m.group("name")
     email = m.group("email")
     return name, email
 
 
-d = toml.load("pyproject.toml")["tool"]["poetry"]
+def get_toml(path=PATH):
+    return toml.load("path)["tool"]["poetry"]
+
+
+d = get_toml()
 project = d["name"]
-author_name, author_email = get_author(d["authors"][0])
+version = d["version"]
+author_name, author_email = extract_author(d["authors"][0])
